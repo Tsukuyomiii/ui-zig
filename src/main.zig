bb30691263a3810bdc96b75204297f5134ee25cc
auri <key.coin.candle@gmail.com>
2025-04-28 02:49:03 -0500

    sync

--- src/main.zig
+++ src/main.zig
@@ -1,51 +1,65 @@
 const std = @import("std");
 const raylib = @import("raylib");
 
-const Box = struct {
-    x: i32,
-    y: i32,
-    width:  i32,
-    height: i32,
-    pub fn draw(self: @This()) void {
-        raylib.drawRectangle(self.x, self.y, self.height, self.width, raylib.Color.blue);
-    }
+const Context = struct {
+    const DragState = struct {
+        id: u32,
+        start_pos: struct { x: i32, y: i32 }, 
+    };
+    elements: struct {
+        data: [20]Element,
+    },
+    drag_states: [10]DragState,
 };
 
 const Element = union(enum) {
+    const Box = struct {
+        id: u32,
+        x: i32,
+        y: i32,
+        width:  i32,
+        height: i32,
+        pub fn draw(this: Box) void {
+            raylib.drawRectangle(this.x, this.y, this.height, this.width, raylib.Color.blue);
+        }
+        pub fn new() Box {
+
+        }
+    };
     box: Box,
 };
-
-
 
 pub fn main() !void {
     raylib.initWindow(800, 600, "UI");
     raylib.setTargetFPS(144);
 
-    const box = Box { 
+    const context: Context = .{
+        .elements = .{
+            .data = undefined,
+        },
+        .drag_states = undefined,
+    };
+
+    const box = Element.Box {
+        .id = std.crypto.random.int(u32),
         .x = 0,
         .y = 0,
         .width  = 100,
         .height = 100,
     };
 
-    const elements = [_]Element{
-        Element{.box = box}
-    };
-
-    if (raylib.isMouseButtonDown(.left)) {
-        const mx = raylib.getMouseX();
-        const my = raylib.getMouseY();
-        if (mx >= box.x and mx <= (box.x + box.width) and my >= box.y and my <= (box.y + box.height)) {
-
-        }
-    }
-    
-
     while (!raylib.windowShouldClose()) {
         raylib.beginDrawing();
         raylib.clearBackground(raylib.Color.dark_gray);
         raylib.drawFPS(0, 0);
-        for (elements) |element| {
+        if (raylib.isMouseButtonDown(.left)) {
+            const mx = raylib.getMouseX();
+            const my = raylib.getMouseY();
+            if (mx >= box.x and mx <= (box.x + box.width) and my >= box.y and my <= (box.y + box.height)) {
+                
+            }
+        }
+        for (context.elements) |element| {
             element.draw();
         }
         raylib.endDrawing();
